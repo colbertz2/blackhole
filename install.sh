19555a73b1f0b3d26eb5c3d34e5371f3e95cd3f7
Zach Colbert <zach.colbert20@gmail.com>
2020-11-27 23:28:36 -0800

    Complete install.sh
    Tested locally, need to test running via curl

--- install.sh
+++ install.sh
@@ -1,3 +1,37 @@
 #!/bin/sh
 
-# TODO
+if [ $EUID -ne 0 ]; then
+    >&2 echo "Please run as root!"
+    exit 1
+fi
+
+SCRIPT_DEST="/usr/local/bin"
+MAN_DEST="/usr/local/man/man1"
+README_DEST="/usr/local/share/blackhole"
+LICENSE_DEST="$README_DEST"
+
+CONF_FILE="/usr/local/etc/blackhole.conf"
+
+# Prep destination folders
+mkdir -p "$SCRIPT_DEST"
+mkdir -p "$MAN_DEST"
+mkdir -p "$README_DEST"
+mkdir -p "$LICENSE_DEST"
+
+# Grab source files from GitHub
+trap "rm -f blackhole blackhole.1.gz LICENSE.txt README.md" SIGINT
+curl -o blackhole "https://raw.githubusercontent.com/colbertz2/blackhole/main/blackhole"
+curl -o blackhole.1.gz "https://raw.githubusercontent.com/colbertz2/blackhole/main/blackhole.1.gz"
+curl -o README.md "https://raw.githubusercontent.com/colbertz2/blackhole/main/README.md"
+curl -o LICENSE.txt "https://raw.githubusercontent.com/colbertz2/blackhole/main/LICENSE.txt"
+
+# Install files
+mv blackhole $SCRIPT_DEST
+mv blackhole.1.gz $MAN_DEST
+mv README.md $README_DEST
+mv LICENSE.txt $LICENSE_DEST
+
+# Create empty conf file
+echo "# Domain blacklist for blackhole CLI" > $CONF_FILE
+
+exit 0
